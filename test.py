2bfa2843f6563e2c0fe7c34059b8b4075a62fdb6
284772894 <284772894@qq.com>
2018-12-27 10:01:44 +0800

    rename directory

--- test.py
+++ test.py
@@ -4,15 +4,15 @@ import requests
 def get_session():
     req = requests.session()
     data = {"username": "test", "pwd": "123456"}
-    req.post("http://127.0.0.1:8080/api/login", data, verify=False)
+    req.post("http://127.0.0.1:8000/api/login", data, verify=False)
     return req
 
 
 def get_api():
     req = get_session()
-    res = req.get(url="http://127.0.0.1:8080/api/get_shop_list", params={"id": "1", "name": "2"})
+    res = req.get(url="http://127.0.0.1:8000/api/get_shop_list", params={"id": "1", "name": "2"})
     print("get_shop_list=%s" % res.text)
-    res2 = req.post(url="http://127.0.0.1:8080/api/save_shop", data={"id": "3", "name": "4"})
+    res2 = req.post(url="http://127.0.0.1:8000/api/save_shop", data={"id": "3", "name": "4"})
     print("save_shop=%s" % res2.text)
 
 
